9c911d3ba31bfad7f3be02b9dd8da7a9bb2e3a98
Quentin Peschard <qpeschard49@gmail.com>
2021-08-12 16:46:28 +0200

    finished exercise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  resources :lists, only: %i[index show new create]
+  get 'bookmarks/new'
+  get 'bookmarks/create'
+  resources :lists, only: %i[index show new create] do
+    resources :bookmarks, only: %i[new create]
+  end
+  resources :bookmarks, only: :destroy
 end
