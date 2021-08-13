888e4c16e451ad677a4946a14131740907d176f3
Quentin Peschard <qpeschard49@gmail.com>
2021-08-13 11:27:47 +0200

    index route change

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   get 'bookmarks/new'
   get 'bookmarks/create'
-  root to: 'lists#index', as: 'lists'
-  resources :lists, only: %i[show new create] do
+  resources :lists, only: %i[index show new create] do
     resources :bookmarks, only: %i[new create]
   end
   resources :bookmarks, only: :destroy
